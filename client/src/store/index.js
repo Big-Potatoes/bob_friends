1b0fbefeecda61c5ef94968168d23b4ee8fe126f
Mmaryyy <marynrvly@gmail.com>
2023-03-22 10:08:09 +0900

    [chore] 초기 설정, 파일 구조 수정

---
+++ client/src/store/index.js
@@ -0,0 +1,29 @@
+import { configureStore, createSlice } from '@reduxjs/toolkit'
+
+// useState 와 비슷한 역할
+// createSlice({
+//     name: 'state이름',
+//     initialState:'값'
+// })
+
+const stock = createSlice({
+  name: 'user',
+  initialState: [
+    {
+      p_id: 0,
+      brand: '드롭드롭드롭',
+      title: '소프트쉬폰 커튼 SAILING TO THE FOREST',
+      cost: '84,000',
+      deliveryFee: '3000',
+    },
+  ],
+})
+
+export default configureStore({
+  // state 등록
+  reducer: {
+    // 작명: user.reducer
+    // user: user.reducer,
+    stock: stock.reducer,
+  },
+})
