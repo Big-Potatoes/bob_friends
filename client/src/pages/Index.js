94f9ccf699c69e7e8319b73c97f3850b5f863fb7
Mmaryyy <marynrvly@gmail.com>
2023-03-25 17:39:32 +0900

    [chore, feat] axios custom

--- client/src/pages/Index.js
+++ client/src/pages/Index.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { OuterWrapper } from '../styles/common'
 import { MainWrapper } from '../styles'
 import Search from '../components/Search'
-
+import { api } from '../api/customAxios'
 const Index = () => {
+  const PAGE_SIZE = 5
+  const [pageNumber, setPageNumber] = useState(1)
+  const [list, setList] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  useEffect(() => {
+    setPageNumber(1)
+    api
+      .get(`/recruit-contents?pageNumber=${pageNumber}&pageSize=${PAGE_SIZE}`)
+      .then((res) => {
+        setList(res.data.content)
+        setIsLoading(false)
+      })
+  }, [])
   return (
     <OuterWrapper>
       <MainWrapper className="landing_page">
@@ -11,10 +24,17 @@ const Index = () => {
         <section className="section_container">
           <div className="write_banner">게시글 작성 배너</div>
           <ul className="content_list">
-            <li className="content1">게시글1</li>
-            <li className="content2">게시글2</li>
-            <li className="content3">게시글3</li>
-            <li className="content3">게시글3</li>
+            {!isLoading ? (
+              list.map((el, idx) => {
+                return (
+                  <li className={`content${idx + 1}`} key={idx}>
+                    {el.title}
+                  </li>
+                )
+              })
+            ) : (
+              <p>로딩중</p>
+            )}
           </ul>
         </section>
       </MainWrapper>
