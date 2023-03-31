66233b599929c624dc1293b74ef9665ad128e670
yjkim <k.yeojin15@gmail.com>
2023-03-31 19:05:20 +0900

    [Feat] detail page init

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,9 +1,27 @@
-/* eslint-disable */
 import React from 'react'
+import { OuterWrapper, Tag } from '../styles/s-global/common'
+import { TagWrap, DetailHead, DetailUser } from '../styles/s-pages/detail'
 
 const Detail = () => {
   return (
-    <div>Detail</div>
+    <OuterWrapper>
+      <TagWrap>
+        <Tag>치킨</Tag>
+        <Tag>또래오래</Tag>
+      </TagWrap>
+      <DetailHead>
+        <h2>
+          또래오래 같이 드실 분 감자 아파트로 오세여 사주는 건 아님 엔빵이에요
+        </h2>
+        <DetailUser>
+          <p>수미감자칩</p>
+          <div>
+            <span>2023.10.10 19:00</span>
+            <span>20분 전</span>
+          </div>
+        </DetailUser>
+      </DetailHead>
+    </OuterWrapper>
   )
 }
 
