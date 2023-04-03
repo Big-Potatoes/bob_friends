ae954d0564e5f2906ef1bd3d770bf76d988b4323
Mmaryyy <marynrvly@gmail.com>
2023-04-03 23:50:36 +0900

    [style, feat] 계정 찾기, 소셜로그인 작업 순위 변경으로 UI 삭제

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -15,10 +15,10 @@ import {
 } from '../styles/s-pages/login'
 
 const Login = () => {
-  const [isValid, setIsValid] = useState(false)
+  const [isValid, setIsValid] = useState(true)
   const checkValid = () => {
     // 로그인 버튼 누르면 서버에 post 요청 -> 회원 정보 상태에 따라 isValid 변경
-    setIsValid(true)
+    setIsValid(false)
   }
   return (
     <OuterWrapper>
@@ -30,7 +30,7 @@ const Login = () => {
         <InputContainer className="input_password">
           <InputLabel htmlFor="password">비밀번호</InputLabel>
           <InputBase type="password" id="password"></InputBase>
-          {isValid ? (
+          {!isValid ? (
             <AlertText>
               아이디가 유효하지 않거나 잘못된 비밀번호 입니다.
             </AlertText>
@@ -43,11 +43,11 @@ const Login = () => {
           <CustomSpan>아직 계정이 없나요?</CustomSpan>
           <CustomLink to="/join">회원가입</CustomLink>
         </BottomWrapper>
-        <BottomWrapper className="identify">
+        {/* <BottomWrapper className="identify">
           <CustomSpan>아이디/비밀번호를 잊었나요?</CustomSpan>
           <CustomLink to="identify">계정찾기</CustomLink>
-        </BottomWrapper>
-        <span>소셜로그인</span>
+        </BottomWrapper> */}
+        {/* <span>소셜로그인</span> */}
       </Wrapper>
     </OuterWrapper>
   )
