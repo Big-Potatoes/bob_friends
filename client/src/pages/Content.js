eaea5cbf8035e8ba60f15674b0f24378d741ce28
yjkim <k.yeojin15@gmail.com>
2023-04-05 22:55:45 +0900

    [Chore, Feat] dayjs 설치, 컨텐츠 내용 추가

--- client/src/pages/Content.js
+++ client/src/pages/Content.js
@@ -1,11 +1,124 @@
-import React from 'react'
-import { OuterWrapper, Tag } from '../styles/s-global/common'
+/* eslint-disable */
+import React, { useEffect, useState } from 'react'
+import { api } from '../api/api'
+import { OuterWrapper, Tag, ButtonSm } from '../styles/s-global/common'
+import {
+  TagWrap,
+  ContentHead,
+  ContentTitle,
+  ContentUser,
+  ContentConsole,
+  ContentField,
+  ContentFieldBox,
+  ContentBtnWrap,
+  ContentText,
+} from '../styles/s-pages/content'
+import {
+  PeopleCountWrapper,
+  ChartWrapper,
+  CountNum,
+} from '../styles/s-components/listcontent'
+import PeopleCountPie from '../components/PeopleCountPie'
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import 'dayjs/locale/ko'
+dayjs.extend(relativeTime)
+dayjs.locale('ko')
 
 const Content = () => {
+  const data = [
+    {
+      id: 'peopleCount',
+      value: 3,
+      color: `var(--maincolor)`,
+    },
+    {
+      id: 'leftPeopleCount',
+      value: 1,
+      color: `var(--black-100)`,
+    },
+  ]
+  const [id] = useState(1)
+  const [contentData, setContentData] = useState([])
+  const [tags, setTags] = useState([])
+  const [deliveryPrice, setDeliveryPrice] = useState(0)
+  const [endTime, setEndTime] = useState('')
+  const [totalPeople, setTotalPeople] = useState(0)
+  const [recruitEnd, setRecruitEnd] = useState(false)
+  const currentTime = dayjs().format('YYYY-MM-DD HH:mm:ss')
+
+  useEffect(() => {
+    api.get(`/recruit-content/${id}`).then((res) => {
+      setContentData(res.data)
+      setTags(res.data.tags)
+      setEndTime(res.data.endDateTime)
+      setDeliveryPrice(res.data.deliveryPrice.totalPrice)
+      setTotalPeople(res.data.deliveryPrice.peopleCount)
+      if (currentTime >= endTime) setRecruitEnd(true)
+    })
+  }, [])
+  const unitCalc = (amount) => {
+    return amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+  }
+  const cutSeconds = (time) => {
+    return time.slice(0, -3)
+  }
+
+  console.log(contentData)
+
   return (
     <OuterWrapper>
-      <Tag>치킨</Tag>
-      <Tag>또래오래</Tag>
+      <TagWrap>
+        {tags.map((item, idx) => (
+          <Tag key={idx}>{item}</Tag>
+        ))}
+      </TagWrap>
+      <ContentHead>
+        <ContentTitle>{contentData.title}</ContentTitle>
+        <PeopleCountWrapper className="people_count">
+          <ChartWrapper className="chart_wrapper">
+            <PeopleCountPie clasName="chart" data={data} />
+            <CountNum className="chart_summary">{`3/4`}</CountNum>
+          </ChartWrapper>
+        </PeopleCountWrapper>
+      </ContentHead>
+      <ContentUser>
+        <h3>{contentData.writer}</h3>
+        <div>
+          <p>
+            {contentData.createDateTime &&
+              cutSeconds(contentData.createDateTime)}
+          </p>
+          <span>{dayjs(contentData.createDateTime).fromNow()}</span>
+        </div>
+      </ContentUser>
+      <ContentConsole>
+        <ContentField>
+          <ContentFieldBox>
+            <span>모집 종료 시간</span>
+            <p>
+              <span>{cutSeconds(endTime)}</span>
+              <span className="notice">
+                {recruitEnd ? '모집완료' : dayjs(endTime).toNow()}
+              </span>
+            </p>
+          </ContentFieldBox>
+          <ContentFieldBox>
+            <span>배달비 개인 부담금</span>
+            <p>{`${unitCalc(deliveryPrice)}원/${totalPeople}명 = ${unitCalc(
+              deliveryPrice / totalPeople
+            )}원`}</p>
+          </ContentFieldBox>
+        </ContentField>
+        <ContentBtnWrap>
+          <ButtonSm>참여하기</ButtonSm>
+          <ButtonSm>
+            내 친구 <br />
+            초대하기
+          </ButtonSm>
+        </ContentBtnWrap>
+      </ContentConsole>
+      <ContentText>{contentData.content}</ContentText>
     </OuterWrapper>
   )
 }
--- client/src/styles/s-pages/content.js
+++ client/src/styles/s-pages/content.js
@@ -7,30 +7,80 @@ export const TagWrap = styled.div`
   align-items: flex-start;
   flex-wrap: wrap;
   ${Tag} {
-    margin-right: 5px;
+    margin-right: 4px;
   }
 `
-export const DetailHead = styled.div`
-  h2 {
-    font-size: var(--fz-lg);
-    font-weight: var(--fw-strong);
-    margin: 15px 0 20px;
-  }
+export const ContentHead = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  margin: 15px 0;
 `
-export const DetailUser = styled.div`
+export const ContentTitle = styled.h2`
+  font-size: var(--fz-lg);
+  line-height: 1.3;
+  width: 70%;
+  font-weight: var(--fw-strong);
+`
+export const ContentUser = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  p {
-    font-size: var(--fz-sm);
+  h3 {
+    font-size: var(--fz-base);
     font-weight: var(--fw-strong);
   }
   div {
-    flex: none;
+    font-size: var(--fz-xs);
+    color: var(--black-300);
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    span {
+      padding-left: 10px;
+    }
   }
+`
+export const ContentConsole = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  margin: 30px 0;
+`
+export const ContentField = styled.div``
+export const ContentFieldBox = styled.div`
+  margin-bottom: 15px;
   span {
     font-size: var(--fz-xs);
     color: var(--black-200);
-    padding-left: 10px;
   }
+  p {
+    margin-top: 7px;
+    color: var(--black-300);
+    font-size: var(--fz-xs);
+    font-weight: var(--fw-strong);
+    span {
+      color: inherit;
+      font-size: inherit;
+      font-weight: inherit;
+    }
+    > .notice {
+      color: var(--maincolor);
+      font-weight: var(--fw-strong);
+      padding-left: 10px;
+    }
+  }
+`
+export const ContentBtnWrap = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: flex-start;
+`
+export const ContentText = styled.div`
+  font-size: var(--fz-base);
+  line-height: 1.5;
+  border: 1px solid var(--black-200);
+  border-radius: 6px;
+  padding: 15px;
+  min-height: 200px;
 `
