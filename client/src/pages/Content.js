0acdf68fba04f4cd5ae8955916583cf952c5f025
yjkim <k.yeojin15@gmail.com>
2023-04-23 15:01:43 +0900

    [feat] 주문한 메뉴 리스트 추가

--- client/src/pages/Content.js
+++ client/src/pages/Content.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { api } from '../api/api'
@@ -35,8 +36,9 @@ const Content = () => {
   const { id } = useParams()
   const [contentData, setContentData] = useState([])
   const [recruitEnd, setRecruitEnd] = useState(false)
-  const currentTime = dayjs().format('YYYY-MM-DD HH:mm:ss')
 
+  const currentTime = dayjs().format('YYYY-MM-DD HH:mm:ss')
+  console.log(contentData)
   useEffect(() => {
     api.get(`/recruit-content/${id}`).then((res) => {
       setContentData(res.data)
@@ -65,130 +67,135 @@ const Content = () => {
 
   return (
     <OuterWrapper>
-      <TagWrap>
-        {contentData.tags &&
-          contentData.tags.map((item, idx) => <Tag key={idx}>{item}</Tag>)}
-      </TagWrap>
-      <ContentHead>
-        <ContentTitle>{contentData.title}</ContentTitle>
-        <PeopleCountPie
-          data={chartData}
-          peopleCount={contentData.peopleCount}
-          totalPeopleCount={contentData.totalPeopleCount}
-        />
-      </ContentHead>
-      <ContentUser>
-        <h3>{contentData.writer}</h3>
-        <div>
-          <p>
-            {contentData.createDateTime &&
-              cutSeconds(contentData.createDateTime)}
-          </p>
-          <span>{dayjs(contentData.createDateTime).fromNow()}</span>
-        </div>
-      </ContentUser>
-      <ContentConsole>
-        <div>
-          <ContentFieldBox>
-            <ContentSubject>모집 종료 시간</ContentSubject>
+      <div className="content-wrap">
+        <TagWrap>
+          {contentData.tags &&
+            contentData.tags.map((item, idx) => <Tag key={idx}>{item}</Tag>)}
+        </TagWrap>
+        <ContentHead>
+          <ContentTitle>{contentData.title}</ContentTitle>
+          <PeopleCountPie
+            data={chartData}
+            peopleCount={contentData.peopleCount}
+            totalPeopleCount={contentData.totalPeopleCount}
+          />
+        </ContentHead>
+        <ContentUser>
+          <h3>{contentData.writer}</h3>
+          <div>
             <p>
-              <span>
-                {contentData.endDateTime && cutSeconds(contentData.endDateTime)}
-              </span>
-              <span className="notice">
-                {recruitEnd
-                  ? '모집완료'
-                  : dayjs(contentData.endDateTime).toNow()}
-              </span>
+              {contentData.createDateTime &&
+                cutSeconds(contentData.createDateTime)}
             </p>
-          </ContentFieldBox>
-          <ContentFieldBox>
-            <ContentSubject>배달비 개인 부담금</ContentSubject>
-            <p>
-              {contentData.deliveryPrice &&
-                `${unitCalc(contentData.deliveryPrice.totalPrice)}원/${
-                  contentData.totalPeopleCount
-                }명 = ${unitCalc(
-                  contentData.deliveryPrice.totalPrice /
+            <span>{dayjs(contentData.createDateTime).fromNow()}</span>
+          </div>
+        </ContentUser>
+        <ContentConsole>
+          <div>
+            <ContentFieldBox>
+              <ContentSubject>모집 종료 시간</ContentSubject>
+              <p>
+                <span>
+                  {contentData.endDateTime &&
+                    cutSeconds(contentData.endDateTime)}
+                </span>
+                <span className="notice">
+                  {recruitEnd
+                    ? '모집완료'
+                    : dayjs(contentData.endDateTime).toNow()}
+                </span>
+              </p>
+            </ContentFieldBox>
+            <ContentFieldBox>
+              <ContentSubject>배달비 개인 부담금</ContentSubject>
+              <p>
+                {contentData.deliveryPrice &&
+                  `${unitCalc(contentData.deliveryPrice)}원/${
                     contentData.totalPeopleCount
-                )}원`}
-            </p>
-          </ContentFieldBox>
-        </div>
-        <ContentBtnWrap>
-          <ContentBtn paddingRight={`10px`}>
-            참여하기
-            <img src="/assets/spoon.png" alt="참여하기" />
-          </ContentBtn>
-          <ContentBtn>
-            내 친구 <br />
-            초대하기
-            <img src="/assets/spoons.png" alt="친구초대하기" />
-          </ContentBtn>
-        </ContentBtnWrap>
-      </ContentConsole>
-      <ContentText>{contentData.content}</ContentText>
-      <ContentArea>
-        <ContentAreaBox>
-          <ContentSubject>지점 정보</ContentSubject>
-          <MapContainer
-            mapid={`map_store_${contentData.id}`}
-            mapLocation={
-              contentData.storeLocation && contentData.storeLocation.address
-            }
-            lat={
-              contentData.storeLocation && contentData.storeLocation.latitude
-            }
-            lng={
-              contentData.storeLocation && contentData.storeLocation.longitude
-            }
-          />
-        </ContentAreaBox>
-        <ContentAreaBox>
-          <ContentSubject>
-            밥 친구가 주문한 메뉴
-            <Tooltip>
-              <FaQuestionCircle />
-              <TooltipMsg>
-                메뉴가 고민되나요?
-                <br />
-                밥친구들의 주문을 확인해 보세요!
-              </TooltipMsg>
-            </Tooltip>
-          </ContentSubject>
-          <FriendsMenu>
-            {contentData.menus &&
-              contentData.menus.map((item, idx) => <li key={idx}>{item}</li>)}
-          </FriendsMenu>
-          <MenuStatus>주문 현황</MenuStatus>
-        </ContentAreaBox>
-        <ContentAreaBox>
-          <ContentSubject>픽업 장소</ContentSubject>
-          <MapContainer
-            mapid={`map_pickup_${contentData.id}`}
-            mapLocation={
-              contentData.pickupLocation && contentData.pickupLocation.address
-            }
-            lat={
-              contentData.pickupLocation && contentData.pickupLocation.latitude
-            }
-            lng={
-              contentData.pickupLocation && contentData.pickupLocation.longitude
-            }
-          />
-        </ContentAreaBox>
-        <ContentAreaBox>
-          <ContentSubject>픽업 장소 사진</ContentSubject>
-          <PickupImages>
-            {contentData.pickupLocation &&
-              contentData.pickupLocation.images.map((item, idx) => (
-                <ImageBox key={idx}>
-                  <img src={item} alt="픽업장소사진" className="slide-img" />
-                </ImageBox>
-              ))}
-          </PickupImages>
-        </ContentAreaBox>
-      </ContentArea>
+                  }명 = ${unitCalc(
+                    contentData.deliveryPrice / contentData.totalPeopleCount
+                  )}원`}
+              </p>
+            </ContentFieldBox>
+          </div>
+          <ContentBtnWrap>
+            <ContentBtn paddingRight={`10px`}>
+              참여하기
+              <img src="/assets/spoon.png" alt="참여하기" />
+            </ContentBtn>
+            <ContentBtn>
+              내 친구 <br />
+              초대하기
+              <img src="/assets/spoons.png" alt="친구초대하기" />
+            </ContentBtn>
+          </ContentBtnWrap>
+        </ContentConsole>
+        <ContentText>{contentData.content}</ContentText>
+        <ContentArea>
+          <ContentAreaBox>
+            <ContentSubject>지점 정보</ContentSubject>
+            <MapContainer
+              mapid={`map_store_${contentData.id}`}
+              mapLocation={
+                contentData.storeLocation && contentData.storeLocation.address
+              }
+              lat={
+                contentData.storeLocation && contentData.storeLocation.latitude
+              }
+              lng={
+                contentData.storeLocation && contentData.storeLocation.longitude
+              }
+            />
+          </ContentAreaBox>
+          <ContentAreaBox>
+            <ContentSubject>
+              밥 친구가 주문한 메뉴
+              <Tooltip>
+                <FaQuestionCircle />
+                <TooltipMsg>
+                  메뉴가 고민되나요?
+                  <br />
+                  밥친구들의 주문을 확인해 보세요!
+                </TooltipMsg>
+              </Tooltip>
+            </ContentSubject>
+            <FriendsMenu>
+              {contentData.menus &&
+                contentData.menus.map((item, idx) => <li key={idx}>{item.name}</li>)}
+            </FriendsMenu>
+            <MenuStatus>주문 현황</MenuStatus>
+          </ContentAreaBox>
+          <ContentAreaBox>
+            <ContentSubject>픽업 장소</ContentSubject>
+            <MapContainer
+              mapid={`map_pickup_${contentData.id}`}
+              mapLocation={
+                contentData.pickupLocation && contentData.pickupLocation.address
+              }
+              lat={
+                contentData.pickupLocation &&
+                contentData.pickupLocation.latitude
+              }
+              lng={
+                contentData.pickupLocation &&
+                contentData.pickupLocation.longitude
+              }
+            />
+          </ContentAreaBox>
+          <ContentAreaBox>
+            <ContentSubject>픽업 장소 사진</ContentSubject>
+            <PickupImages>
+              {contentData.pickupLocation &&
+                contentData.pickupLocation.images.map((item, idx) => (
+                  <ImageBox key={idx}>
+                    <img src={item} alt="픽업장소사진" className="slide-img" />
+                  </ImageBox>
+                ))}
+            </PickupImages>
+          </ContentAreaBox>
+        </ContentArea>
+      </div>
+      <div className="modal-wrap"></div>
     </OuterWrapper>
   )
 }
